4429fab64bcc970ebdc4711d859807f1fb3d9785
Benjamin <vigie.benjamin@outlook.com>
2020-08-23 20:39:23 -0400

    Adding a bookamekers list. If a booky is unavailable, try to get odds from another one

--- src/services/FootballAPI.ts
+++ src/services/FootballAPI.ts
@@ -1,12 +1,20 @@
 import Axios from "axios";
-import { IApiErrorResponse, IApiStatus, IApiStatusResponse, IApiFixturesResponse, IFixture, IApiPredictionsResponse, IApiBookmakerOddsResponse, IBookmakerBet, IBetValues, IApiRoundsResponse } from "./types";
+import * as chalk from "chalk";
+// tslint:disable-next-line: max-line-length
+import { IApiErrorResponse, IApiStatus, IApiStatusResponse, IApiFixturesResponse, IFixture, IApiPredictionsResponse, IApiBookmakerOddsResponse, IBookmakerBet, IBetValues, IApiRoundsResponse, IBookmaker } from "./types";
+import ScoutBot from "../ScoutBot";
+import Tools from "./Tools";
 
 /** Football API URL */
 const FOOTBALL_API_URL = "https://v2.api-football.com";
 /** ID of Ligue 1 Championship. Could be retrieved by the API bu it'll cost 1 API call and it will be the same for 2020/2021 season */
 const LIGUE_1_ID = 2664;
-/** BWIN bookmaker ID */
-const BOOKMAKER_ID = 6;
+/** Bokmakers list */
+const BOOKMAKERS: IBookmaker[] = [
+  { bookmaker_id: 6, bookmaker_name: "BWIN" },
+  { bookmaker_id: 1, bookmaker_name: "10bet" },
+  { bookmaker_id: 11, bookmaker_name: "1xbet" },
+];
 
 /** The FootballApi service manages calls to Football API rest API */
 export default class FootballApi {
@@ -52,9 +60,14 @@ export default class FootballApi {
 
   /** Get all games for the current round */
   async getCurrentRoundGames(): Promise<IFixture[]> {
+    this.displayAction("Retrieving current round");
     const roundResponse = await this.performsFootballApiCall<IApiRoundsResponse>(`/fixtures/rounds/${LIGUE_1_ID}/current`);
+    this.displayAction("Retrieving current round", true, !!roundResponse.fixtures.length);
     const round = roundResponse.fixtures[0];
+
+    this.displayAction("Retrieve round games");
     const apiResponse = await this.performsFootballApiCall<IApiFixturesResponse>(`/fixtures/league/${LIGUE_1_ID}/${round}?timezone=Europe/Paris`);
+    this.displayAction("Retrieve round games", true, !!apiResponse.fixtures.length);
     return apiResponse.fixtures;
   }
 
@@ -63,16 +76,35 @@ export default class FootballApi {
     // tslint:disable-next-line: prefer-for-of
     for (let i = 0; i < games.length; i++) {
       // Retrieve pronostics and attach them
+      this.displayAction(`Retrieving ${games[i].homeTeam.team_name} - ${games[i].awayTeam.team_name} predictions`);
       const pronosticsResponse = await this.performsFootballApiCall<IApiPredictionsResponse>(`/predictions/${games[i].fixture_id}`);
+      this.displayAction(`Retrieving ${games[i].homeTeam.team_name} - ${games[i].awayTeam.team_name} predictions`, true, !!pronosticsResponse.predictions);
       if (pronosticsResponse.predictions[0]) {
         games[i].pronostics = pronosticsResponse.predictions[0];
       }
 
       // Do the same for odds
-      const bookiesResponse = await this.performsFootballApiCall<IApiBookmakerOddsResponse>(`/odds/fixture/${games[i].fixture_id}/bookmaker/${BOOKMAKER_ID}`);
-      if (bookiesResponse.odds[0] && bookiesResponse.odds[0].bookmakers[0]) {
-        games[i].odds = bookiesResponse.odds[0].bookmakers[0];
+      await this.getBookmakerOdds(games[i], 0);
+    }
+  }
+
+  /** Retrieve bookmakers odds. If it fails, try the next bookie until we just have to stop :D */
+  private async getBookmakerOdds(game: IFixture, bookmaker: number): Promise<void> {
+    this.displayAction(`Retrieving ${game.homeTeam.team_name} - ${game.awayTeam.team_name} bookmakers (${BOOKMAKERS[bookmaker].bookmaker_name}) odds`);
+    const bookiesResponse = await this.performsFootballApiCall<IApiBookmakerOddsResponse>(`/odds/fixture/${game.fixture_id}/bookmaker/${BOOKMAKERS[bookmaker].bookmaker_id}`);
+    if (bookiesResponse.odds[0] && bookiesResponse.odds[0].bookmakers[0]) {
+      game.odds = bookiesResponse.odds[0].bookmakers[0];
+    }
+
+    const retrieved = (game.odds !== undefined);
+    this.displayAction(`Retrieving ${game.homeTeam.team_name} - ${game.awayTeam.team_name} bookmakers (${BOOKMAKERS[bookmaker].bookmaker_name}) odds`, true, retrieved);
+
+    // If the current bookmaker is unavailable, try another one
+    if (!retrieved) {
+      if (++bookmaker < BOOKMAKERS.length) {
+        return await this.getBookmakerOdds(game, bookmaker);
       }
+      console.error(chalk`{bgRed No bookmaker's odds available.}`);
     }
   }
 
@@ -82,5 +114,16 @@ export default class FootballApi {
       if (odd.label_name === oddType) return odd.values;
     }
     return [];
+  }
+
+  /** Diaply loading message if debug mode */
+  private displayAction(message: string, done?: boolean, isOk?: boolean): void {
+    if (!ScoutBot.configuration.debugTrace) return;
+
+    if (!done) Tools.writeConsole(chalk`{yellow   ⚡} {gray ${message}...}`);
+    else {
+      if (isOk) Tools.writeConsole(chalk`{green   ✔️} {gray ${message}\n}`, true);
+      else Tools.writeConsole(chalk`{red  ❗️} {gray ${message}\n}`, true);
+    }
   }
 }
